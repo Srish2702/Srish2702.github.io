424e9617231d6f95d6d3a83a207d7a905acae94c
MrNamra <namraramsha@yahoo.com>
2025-04-13 16:04:50 +0530

    dynamic about me

--- script.js
+++ script.js
@@ -129,6 +129,17 @@
         });
     
         $.ajax({
+            url: 'https://srishti.raju.serv00.net/api/about',
+            type: 'get',
+            success: function(response) {
+                const data = response;
+                $('.about-me').append(`
+                    <h2>About Me</h2>
+                    ${data}
+                `);
+            }
+        });
+        $.ajax({
             url: 'https://srishti.raju.serv00.net/api/feedbacks',
             type: 'get',
             success: function(response) {
@@ -165,7 +176,7 @@
                     $portfolioGrid.empty();
                     $portfolioGrid.append(`
                         <div class="portfolio-item" data-id="${item.id}">
-                            <img src="${item.coverpic}" alt="${item.title}">
+                            <img src="${item.coverpic}" alt="image is Loading...">
                             <!-- <div class="portfolio-overlay">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="eye" class="lucide lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"></path><circle cx="12" cy="12" r="3"></circle></svg>
                             </div> -->
